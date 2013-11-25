964a35c4bc65a361440ffa859676971c0ca3d0d3
Mike Sinkovsky <msink@permonline.ru>
2013-11-25 12:43:42 +0600

    main: build_number

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -18,41 +18,21 @@ package com.android.settings;
 
 import android.net.sip.SipManager;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
-import android.telephony.TelephonyManager;
 
 public class Settings extends PreferenceActivity {
 
-    private static final String KEY_PARENT = "parent";
-    private static final String KEY_CALL_SETTINGS = "call_settings";
-    private static final String KEY_SYNC_SETTINGS = "sync_settings";
-    private static final String KEY_DOCK_SETTINGS = "dock_settings";
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         addPreferencesFromResource(R.xml.settings);
         
-        int activePhoneType = TelephonyManager.getDefault().getPhoneType();
-
-        PreferenceGroup parent = (PreferenceGroup) findPreference(KEY_PARENT);
-        Utils.updatePreferenceToSpecificActivityOrRemove(this, parent, KEY_SYNC_SETTINGS, 0);
-
-        Preference dockSettings = parent.findPreference(KEY_DOCK_SETTINGS);
-        if (getResources().getBoolean(R.bool.has_dock_settings) == false && dockSettings != null) {
-            parent.removePreference(dockSettings);
-        }
-    }
-    
-    @Override
-    protected void onResume() {
-        super.onResume();
-        findPreference(KEY_CALL_SETTINGS).setEnabled(
-                !AirplaneModeEnabler.isAirplaneModeOn(this)
-                || SipManager.isVoipSupported(this));
+        findPreference("build_number")
+            .setSummary(SystemProperties.get("product.version", "UNKNOWN"));
     }
 
 }
