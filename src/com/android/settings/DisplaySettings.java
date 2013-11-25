53cf170a2227bcad8c4a94e4748442ceb2f34cbf
Mike Sinkovsky <msink@permonline.ru>
2013-11-25 12:48:42 +0600

    display settings

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -16,7 +16,9 @@
 
 package com.android.settings;
 
+import static android.provider.Settings.System.EPD_REFRESH_MODE;
 import static android.provider.Settings.System.SCREEN_OFF_TIMEOUT;
+import static android.provider.Settings.System.AUTO_SHUTDOWN_TIMEOUT;
 
 import java.util.ArrayList;
 
@@ -42,13 +44,9 @@ public class DisplaySettings extends PreferenceActivity implements
     /** If there is no setting in the provider, use this. */
     private static final int FALLBACK_SCREEN_TIMEOUT_VALUE = 30000;
 
+    private static final String KEY_PAGE_REFRESH_RATE     = "page_refresh_rate";
     private static final String KEY_SCREEN_TIMEOUT = "screen_timeout";
-    private static final String KEY_ANIMATIONS = "animations";
-    private static final String KEY_ACCELEROMETER = "accelerometer";
-
-    private ListPreference mAnimations;
-    private CheckBoxPreference mAccelerometer;
-    private float[] mAnimationScales;
+    private static final String KEY_AUTO_SHUTDOWN_TIMEOUT = "auto_shutdown_timeout";
 
     private IWindowManager mWindowManager;
 
@@ -60,143 +58,55 @@ public class DisplaySettings extends PreferenceActivity implements
 
         addPreferencesFromResource(R.xml.display_settings);
 
-        mAnimations = (ListPreference) findPreference(KEY_ANIMATIONS);
-        mAnimations.setOnPreferenceChangeListener(this);
-        mAccelerometer = (CheckBoxPreference) findPreference(KEY_ACCELEROMETER);
-        mAccelerometer.setPersistent(false);
+        ListPreference refresh =
+            (ListPreference) findPreference(KEY_PAGE_REFRESH_RATE);
+        refresh.setValue(String.valueOf
+            (Settings.System.getInt(resolver, EPD_REFRESH_MODE, -1)));
+        refresh.setOnPreferenceChangeListener(this);
 
-        ListPreference screenTimeoutPreference =
+        ListPreference sleep =
             (ListPreference) findPreference(KEY_SCREEN_TIMEOUT);
-        screenTimeoutPreference.setValue(String.valueOf(Settings.System.getInt(
-                resolver, SCREEN_OFF_TIMEOUT, FALLBACK_SCREEN_TIMEOUT_VALUE)));
-        screenTimeoutPreference.setOnPreferenceChangeListener(this);
-        disableUnusableTimeouts(screenTimeoutPreference);
-    }
+        sleep.setValue(String.valueOf
+            (Settings.System.getInt(resolver, SCREEN_OFF_TIMEOUT, FALLBACK_SCREEN_TIMEOUT_VALUE)));
+        sleep.setOnPreferenceChangeListener(this);
 
-    private void disableUnusableTimeouts(ListPreference screenTimeoutPreference) {
-        final DevicePolicyManager dpm =
-            (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-        final long maxTimeout = dpm != null ? dpm.getMaximumTimeToLock(null) : 0;
-        if (maxTimeout == 0) {
-            return; // policy not enforced
-        }
-        final CharSequence[] entries = screenTimeoutPreference.getEntries();
-        final CharSequence[] values = screenTimeoutPreference.getEntryValues();
-        ArrayList<CharSequence> revisedEntries = new ArrayList<CharSequence>();
-        ArrayList<CharSequence> revisedValues = new ArrayList<CharSequence>();
-        for (int i = 0; i < values.length; i++) {
-            long timeout = Long.valueOf(values[i].toString());
-            if (timeout <= maxTimeout) {
-                revisedEntries.add(entries[i]);
-                revisedValues.add(values[i]);
-            }
-        }
-        if (revisedEntries.size() != entries.length || revisedValues.size() != values.length) {
-            screenTimeoutPreference.setEntries(
-                    revisedEntries.toArray(new CharSequence[revisedEntries.size()]));
-            screenTimeoutPreference.setEntryValues(
-                    revisedValues.toArray(new CharSequence[revisedValues.size()]));
-            final int userPreference = Integer.valueOf(screenTimeoutPreference.getValue());
-            if (userPreference <= maxTimeout) {
-                screenTimeoutPreference.setValue(String.valueOf(userPreference));
-            } else {
-                // There will be no highlighted selection since nothing in the list matches
-                // maxTimeout. The user can still select anything less than maxTimeout.
-                // TODO: maybe append maxTimeout to the list and mark selected.
-            }
-        }
-        screenTimeoutPreference.setEnabled(revisedEntries.size() > 0);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        updateState(true);
-    }
-
-    private void updateState(boolean force) {
-        int animations = 0;
-        try {
-            mAnimationScales = mWindowManager.getAnimationScales();
-        } catch (RemoteException e) {
-        }
-        if (mAnimationScales != null) {
-            if (mAnimationScales.length >= 1) {
-                animations = ((int)(mAnimationScales[0]+.5f)) % 10;
-            }
-            if (mAnimationScales.length >= 2) {
-                animations += (((int)(mAnimationScales[1]+.5f)) & 0x7) * 10;
-            }
-        }
-        int idx = 0;
-        int best = 0;
-        CharSequence[] aents = mAnimations.getEntryValues();
-        for (int i=0; i<aents.length; i++) {
-            int val = Integer.parseInt(aents[i].toString());
-            if (val <= animations && val > best) {
-                best = val;
-                idx = i;
-            }
-        }
-        mAnimations.setValueIndex(idx);
-        updateAnimationsSummary(mAnimations.getValue());
-        mAccelerometer.setChecked(Settings.System.getInt(
-                getContentResolver(),
-                Settings.System.ACCELEROMETER_ROTATION, 0) != 0);
-    }
-
-    private void updateAnimationsSummary(Object value) {
-        CharSequence[] summaries = getResources().getTextArray(R.array.animations_summaries);
-        CharSequence[] values = mAnimations.getEntryValues();
-        for (int i=0; i<values.length; i++) {
-            //Log.i("foo", "Comparing entry "+ values[i] + " to current "
-            //        + mAnimations.getValue());
-            if (values[i].equals(value)) {
-                mAnimations.setSummary(summaries[i]);
-                break;
-            }
-        }
+        ListPreference shutdown =
+            (ListPreference) findPreference(KEY_AUTO_SHUTDOWN_TIMEOUT);
+        shutdown.setValue(String.valueOf
+            (Settings.System.getInt(resolver, AUTO_SHUTDOWN_TIMEOUT, -1)));
+        shutdown.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mAccelerometer) {
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.ACCELEROMETER_ROTATION,
-                    mAccelerometer.isChecked() ? 1 : 0);
-        }
         return true;
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
-        if (KEY_ANIMATIONS.equals(key)) {
-            try {
+
+        if (KEY_PAGE_REFRESH_RATE.equals(key)) {
                 int value = Integer.parseInt((String) objValue);
-                if (mAnimationScales.length >= 1) {
-                    mAnimationScales[0] = value%10;
-                }
-                if (mAnimationScales.length >= 2) {
-                    mAnimationScales[1] = (value/10)%10;
-                }
                 try {
-                    mWindowManager.setAnimationScales(mAnimationScales);
-                } catch (RemoteException e) {
-                }
-                updateAnimationsSummary(objValue);
+                Settings.System.putInt(getContentResolver(), EPD_REFRESH_MODE, value);
             } catch (NumberFormatException e) {
-                Log.e(TAG, "could not persist animation setting", e);
+                Log.e(TAG, "could not persist epd refresh setting", e);
             }
 
-        }
-        if (KEY_SCREEN_TIMEOUT.equals(key)) {
+        } else if (KEY_SCREEN_TIMEOUT.equals(key)) {
             int value = Integer.parseInt((String) objValue);
             try {
-                Settings.System.putInt(getContentResolver(),
-                        SCREEN_OFF_TIMEOUT, value);
+                Settings.System.putInt(getContentResolver(), SCREEN_OFF_TIMEOUT, value);
             } catch (NumberFormatException e) {
-                Log.e(TAG, "could not persist screen timeout setting", e);
+                Log.e(TAG, "could not persist sleep timeout setting", e);
+        }
+
+        } else if (KEY_AUTO_SHUTDOWN_TIMEOUT.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            try {
+                Settings.System.putInt(getContentResolver(), AUTO_SHUTDOWN_TIMEOUT, value);
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "could not persist auto shutdown setting", e);
             }
         }
 
